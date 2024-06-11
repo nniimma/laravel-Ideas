c4531c674d8a1db88fbbc7de072274a3975ef639
Nima <alireza.khazforoosh@adventistas.org>
2024-06-11 11:24:58 -0300

    making layout file with: @yield('content') @extends('page') @section('content') @endsection

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,109 +1,61 @@
-<!DOCTYPE html>
-<html lang="EN">
+@extends('layout.layout')
+@section('content')
+    <div class="col-9">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    {{-- icon --}}
-    <link rel="icon" href="{{ URL::asset('img/layout/ideas.png') }}">
-
-    <title>Ideas</title>
-
-    <link href="https://bootswatch.com/5/sketchy/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark ticky-top bg-body-tertiary"
-        data-bs-theme="dark">
-        <div class="container">
-            <a class="navbar-brand fw-light" href="/"><span class="fas fa-brain me-1"> </span>Ideas</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/login">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/register">Register</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/profile">Profile</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="container py-4">
-        <div class="col-9">
-
-            <h1>Terms</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi labore ratione minus, magni quo minima
-            asperiores
-            tempore voluptas eaque beatae dolores doloribus maxime, vero culpa. In provident quas est, voluptas animi
-            qui
-            earum. Praesentium repellendus totam labore, itaque modi animi nostrum molestias ipsa facilis saepe? In
-            numquam
-            maiores repudiandae eum magni rerum doloribus dignissimos ipsum reprehenderit? Sequi deleniti doloremque
-            odit
-            perferendis sint cumque itaque totam. Reprehenderit culpa provident dolor suscipit cupiditate itaque quasi,
-            corporis quaerat corrupti dolores labore consectetur aperiam tempore dolorem fugiat est! Optio ut incidunt
-            totam
-            et blanditiis voluptate, corporis molestias dolore laborum. Sequi quaerat distinctio, sunt labore,
-            necessitatibus nostrum non dolores ex totam iusto optio. Explicabo odit cupiditate recusandae veritatis!
-            Itaque,
-            nobis iure enim asperiores expedita esse ipsam alias quos veniam quidem nulla quibusdam magni unde
-            architecto
-            explicabo voluptatem rem totam necessitatibus ut dolorum. Harum modi mollitia, maxime inventore hic
-            voluptate
-            odit illo debitis deleniti eligendi iste fugit aliquid in ipsa rerum voluptatibus, expedita, cumque delectus
-            saepe. Inventore deleniti quisquam repellendus iure facilis. Saepe architecto soluta sint excepturi
-            cupiditate
-            magnam nostrum dolorum ut debitis nobis quis quaerat doloremque, aspernatur atque in tempora. Consequuntur
-            autem
-            perspiciatis quis est odio totam nemo cupiditate pariatur consequatur. Enim fugiat, ex delectus ab, vero
-            debitis
-            dolore beatae sint ducimus impedit magnam laborum expedita inventore nisi. Repellat magnam quae reiciendis
-            nihil, dolores sed commodi eius deleniti odit autem, dignissimos consectetur eligendi saepe id minima
-            ducimus
-            molestias facere officiis ea? Assumenda voluptatibus amet totam sequi, blanditiis animi ratione suscipit ad
-            molestias nam cupiditate voluptas corrupti harum praesentium doloremque consequuntur fugit repellendus atque
-            aut! Quaerat ipsam veritatis itaque quis perspiciatis doloremque natus ut totam reiciendis officiis eum
-            eveniet
-            quidem ducimus, animi illo asperiores et deleniti sunt consequuntur sapiente ullam aut quo reprehenderit
-            vitae!
-            Quaerat quam blanditiis voluptas eos deleniti perferendis, voluptate consectetur hic provident repellat
-            quibusdam maiores sit non rem tempora fugiat architecto ex fuga reprehenderit, animi illum. Sed et eligendi,
-            molestias praesentium dolores nulla rerum voluptates in natus laudantium quo nihil repellat. Voluptatum
-            molestiae temporibus nulla optio hic ex exercitationem ab dolor ipsam laudantium, minus modi qui, dolorem
-            quos
-            cupiditate incidunt dicta? Doloribus saepe eius voluptas veritatis accusamus natus tempora consectetur ab
-            excepturi voluptate eveniet iusto voluptatum, magnam qui eligendi tempore est. Soluta, ut esse. Ut nisi
-            doloribus provident repellendus aspernatur soluta explicabo illo cum dignissimos? Accusamus voluptates
-            consequatur quia? Modi consectetur, autem corporis laudantium omnis explicabo ipsam! In fugiat et doloremque
-            animi optio, necessitatibus iusto deserunt repudiandae ex dignissimos voluptatum sit, aut libero natus sequi
-            tenetur ea, incidunt perferendis vel veniam? Sit dolorum aspernatur porro odit quibusdam nisi! Aspernatur,
-            dolor
-            magnam assumenda pariatur tenetur expedita mollitia aut, quisquam odit in itaque ex, aperiam accusantium
-            facilis
-            nemo harum. Veniam ad reiciendis nisi quis labore harum architecto nesciunt omnis enim eos voluptatum cum
-            iure
-            recusandae, inventore, nam quia, dolorem quaerat numquam quisquam nulla. Quasi dolorum nam odit alias ipsa
-            nemo
-            repudiandae facilis totam excepturi. Provident optio laudantium a natus dicta architecto quisquam quis earum
-            sunt, ullam nisi voluptates soluta mollitia consequatur labore vel, velit reprehenderit?
-        </div>
+        <h1>Terms</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi labore ratione minus, magni quo minima
+        asperiores
+        tempore voluptas eaque beatae dolores doloribus maxime, vero culpa. In provident quas est, voluptas animi
+        qui
+        earum. Praesentium repellendus totam labore, itaque modi animi nostrum molestias ipsa facilis saepe? In
+        numquam
+        maiores repudiandae eum magni rerum doloribus dignissimos ipsum reprehenderit? Sequi deleniti doloremque
+        odit
+        perferendis sint cumque itaque totam. Reprehenderit culpa provident dolor suscipit cupiditate itaque quasi,
+        corporis quaerat corrupti dolores labore consectetur aperiam tempore dolorem fugiat est! Optio ut incidunt
+        totam
+        et blanditiis voluptate, corporis molestias dolore laborum. Sequi quaerat distinctio, sunt labore,
+        necessitatibus nostrum non dolores ex totam iusto optio. Explicabo odit cupiditate recusandae veritatis!
+        Itaque,
+        nobis iure enim asperiores expedita esse ipsam alias quos veniam quidem nulla quibusdam magni unde
+        architecto
+        explicabo voluptatem rem totam necessitatibus ut dolorum. Harum modi mollitia, maxime inventore hic
+        voluptate
+        odit illo debitis deleniti eligendi iste fugit aliquid in ipsa rerum voluptatibus, expedita, cumque delectus
+        saepe. Inventore deleniti quisquam repellendus iure facilis. Saepe architecto soluta sint excepturi
+        cupiditate
+        magnam nostrum dolorum ut debitis nobis quis quaerat doloremque, aspernatur atque in tempora. Consequuntur
+        autem
+        perspiciatis quis est odio totam nemo cupiditate pariatur consequatur. Enim fugiat, ex delectus ab, vero
+        debitis
+        dolore beatae sint ducimus impedit magnam laborum expedita inventore nisi. Repellat magnam quae reiciendis
+        nihil, dolores sed commodi eius deleniti odit autem, dignissimos consectetur eligendi saepe id minima
+        ducimus
+        molestias facere officiis ea? Assumenda voluptatibus amet totam sequi, blanditiis animi ratione suscipit ad
+        molestias nam cupiditate voluptas corrupti harum praesentium doloremque consequuntur fugit repellendus atque
+        aut! Quaerat ipsam veritatis itaque quis perspiciatis doloremque natus ut totam reiciendis officiis eum
+        eveniet
+        quidem ducimus, animi illo asperiores et deleniti sunt consequuntur sapiente ullam aut quo reprehenderit
+        vitae!
+        Quaerat quam blanditiis voluptas eos deleniti perferendis, voluptate consectetur hic provident repellat
+        quibusdam maiores sit non rem tempora fugiat architecto ex fuga reprehenderit, animi illum. Sed et eligendi,
+        molestias praesentium dolores nulla rerum voluptates in natus laudantium quo nihil repellat. Voluptatum
+        molestiae temporibus nulla optio hic ex exercitationem ab dolor ipsam laudantium, minus modi qui, dolorem
+        quos
+        cupiditate incidunt dicta? Doloribus saepe eius voluptas veritatis accusamus natus tempora consectetur ab
+        excepturi voluptate eveniet iusto voluptatum, magnam qui eligendi tempore est. Soluta, ut esse. Ut nisi
+        doloribus provident repellendus aspernatur soluta explicabo illo cum dignissimos? Accusamus voluptates
+        consequatur quia? Modi consectetur, autem corporis laudantium omnis explicabo ipsam! In fugiat et doloremque
+        animi optio, necessitatibus iusto deserunt repudiandae ex dignissimos voluptatum sit, aut libero natus sequi
+        tenetur ea, incidunt perferendis vel veniam? Sit dolorum aspernatur porro odit quibusdam nisi! Aspernatur,
+        dolor
+        magnam assumenda pariatur tenetur expedita mollitia aut, quisquam odit in itaque ex, aperiam accusantium
+        facilis
+        nemo harum. Veniam ad reiciendis nisi quis labore harum architecto nesciunt omnis enim eos voluptatum cum
+        iure
+        recusandae, inventore, nam quia, dolorem quaerat numquam quisquam nulla. Quasi dolorum nam odit alias ipsa
+        nemo
+        repudiandae facilis totam excepturi. Provident optio laudantium a natus dicta architecto quisquam quis earum
+        sunt, ullam nisi voluptates soluta mollitia consequatur labore vel, velit reprehenderit?
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
