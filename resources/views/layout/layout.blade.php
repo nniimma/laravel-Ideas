149100b2befbbb17bd931644b9094a4fb34f933f
Nima <alireza.khazforoosh@adventistas.org>
2024-06-12 10:43:32 -0300

    renaming header

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -20,7 +20,7 @@
 </head>
 
 <body>
-    @include('layout.nav')
+    @include('layout.header')
 
     <div class="container py-4">
         @yield('content')
@@ -29,6 +29,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
+
     <script src="{{ URL::asset('JS/alpine.js') }}"></script>
 </body>
 
