8da72d3393061314afa33e3b64c9fb0087b513ac
Nima <alireza.khazforoosh@adventistas.org>
2024-06-14 10:24:23 -0300

    update idea form

--- resources/views/ideas/includes/submit-idea.blade.php
+++ resources/views/ideas/includes/submit-idea.blade.php
@@ -1,15 +1,20 @@
-<h4> Share yours ideas </h4>
-<div class="row">
-    <form action="{{ route('idea.store') }}" method="post">
-        @csrf
-        <div class="mb-3">
-            <textarea placeholder="This is my idea..." name="idea" class="form-control" id="idea" rows="3">{{ old('idea') }}</textarea>
-            @error('idea')
-                <p class="error">{{ $message }}</p>
-            @enderror
-        </div>
-        <div class="">
-            <button class="btn btn-dark"> Share </button>
-        </div>
-    </form>
-</div>
+@auth
+    <h4> Share yours ideas </h4>
+    <div class="row">
+        <form action="{{ route('idea.store') }}" method="post">
+            @csrf
+            <div class="mb-3">
+                <textarea placeholder="This is my idea..." name="idea" class="form-control" id="idea" rows="3">{{ old('idea') }}</textarea>
+                @error('idea')
+                    <p class="error">{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="">
+                <button class="btn btn-dark"> Share </button>
+            </div>
+        </form>
+    </div>
+@endauth
+@guest
+    <h1 class="d-flex align-items-center justify-content-center">Login to share your ideas.</h1>
+@endguest
