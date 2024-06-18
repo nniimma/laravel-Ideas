c1182cc3f7c27b77db7ce5cf9f7bb6b289da9058
Nima <alireza.khazforoosh@adventistas.org>
2024-06-18 19:24:04 -0300

    update theme: https://bootswatch.com/5/sketchy/bootstrap.min.css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,9 +7,9 @@
                 @include('includes.success-message')
                 <form class="form mt-5" action="{{ route('login.store') }}" method="post">
                     @csrf
-                    <h3 class="text-center text-dark">Login</h3>
+                    <h3 class="text-center">Login</h3>
                     <div class="form-group">
-                        <label for="email" class="text-dark">Email:</label><br>
+                        <label for="email">Email:</label><br>
                         <input value="{{ old('email') }}" placeholder="example@yahoo.com" type="email" name="email"
                             id="email" class="form-control">
                         @error('email')
@@ -17,18 +17,18 @@
                         @enderror
                     </div>
                     <div class="form-group mt-3">
-                        <label for="password" class="text-dark">Password:</label><br>
+                        <label for="password">Password:</label><br>
                         <input type="password" placeholder="123456" name="password" id="password" class="form-control">
                         @error('password')
                             <p class="error">{{ $message }}</p>
                         @enderror
                     </div>
                     <div class="form-group">
-                        <label for="remember-me" class="text-dark"></label><br>
+                        <label for="remember-me"></label><br>
                         <input type="submit" name="submit" class="btn btn-dark btn-md" value="Login">
                     </div>
                     <div class="text-right mt-2">
-                        <a href="{{ route('register') }}" class="text-dark">Register here</a>
+                        <a href="{{ route('register') }}">Register here</a>
                     </div>
                 </form>
             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,9 +7,9 @@
                 @include('includes.success-message')
                 <form class="form mt-5" action="{{ route('register') }}" method="post">
                     @csrf
-                    <h3 class="text-center text-dark">Register</h3>
+                    <h3 class="text-center">Register</h3>
                     <div class="form-group">
-                        <label for="name" class="text-dark">Name:</label><br>
+                        <label for="name">Name:</label><br>
                         <input value="{{ old('name') }}" placeholder="Joe" type="text" name="name" id="name"
                             class="form-control mb-3">
                         @error('name')
@@ -17,7 +17,7 @@
                         @enderror
                     </div>
                     <div class="form-group">
-                        <label for="email" class="text-dark">Email:</label><br>
+                        <label for="email">Email:</label><br>
                         <input value="{{ old('email') }}" placeholder="example@yahoo.com" type="email" name="email"
                             id="email" class="form-control">
                         @error('email')
@@ -25,7 +25,7 @@
                         @enderror
                     </div>
                     <div class="form-group mt-3">
-                        <label for="password" class="text-dark">Password:</label><br>
+                        <label for="password">Password:</label><br>
                         <input type="password" placeholder="123456" name="password" id="password" class="form-control">
                         @error('password')
                             <p class="error">{{ $message }}</p>
@@ -33,7 +33,7 @@
                     </div>
                     {{-- ! we need to put the name of the password confirm: password_confirmation, in this case we can use the request to validate it: --}}
                     <div class="form-group mt-3">
-                        <label for="confirm-password" class="text-dark">Confirm Password:</label><br>
+                        <label for="confirm-password">Confirm Password:</label><br>
                         <input type="password" placeholder="123456" name="password_confirmation" id="confirm-password"
                             class="form-control">
                         @error('password_confirmation')
@@ -41,11 +41,11 @@
                         @enderror
                     </div>
                     <div class="form-group">
-                        <label for="remember-me" class="text-dark"></label><br>
+                        <label for="remember-me"></label><br>
                         <input type="submit" name="submit" class="btn btn-dark btn-md" value="submit">
                     </div>
                     <div class="text-right mt-2">
-                        <a href="/login" class="text-dark">Login here</a>
+                        <a href="/login">Login here</a>
                     </div>
                 </form>
             </div>
