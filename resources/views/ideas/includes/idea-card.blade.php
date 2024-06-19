c4331baa4847bb33dd489f290b794ad295e03079
Nima <alireza.khazforoosh@adventistas.org>
2024-06-19 16:33:26 -0300

    carbon is the page of laravel for dates: -toDateString() -diffForHumans()

--- resources/views/ideas/includes/idea-card.blade.php
+++ resources/views/ideas/includes/idea-card.blade.php
@@ -46,7 +46,7 @@
             @include('ideas.includes.like-button')
             <div>
                 <span class="fs-6 fw-light text-muted"> <span class="fas fa-clock"> </span>
-                    {{ $idea->created_at }} </span>
+                    {{ $idea->created_at->diffForHumans() }} </span>
             </div>
         </div>
         @include('ideas.includes.comments-box')
