412ae276c087e99167ebb5fd46e2b95e3cf50675
Nima <alireza.khazforoosh@adventistas.org>
2024-06-25 17:14:11 -0300

    caching top users for 3 minutes if you want to clear the cache: -php artisan cache:clear

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,8 +26,16 @@ class AppServiceProvider extends ServiceProvider
         // by default is tailwind but you can change it to any library that you want.
         Paginator::useBootstrapFive();
 
+        // ttl is time to live and it is in seconds:
+        $topUsers = Cache::remember('topUsers', 60 * 3, function () {
+            return User::withCount('ideas')->orderBy('ideas_count', 'DESC')->limit(5)->get();
+        });
+
         // the withCount that we use should be with the name of the function that we use in the model: it will add it to functionName_count (in this case: ideas_count)
         // global variable:
-        View::share("topUsers", User::withCount('ideas')->orderBy('ideas_count', 'DESC')->limit(5)->get());
+        View::share("topUsers", $topUsers);
+
+        //! to clear the cache: 
+        //todo: Cache::flush();
     }
 }
