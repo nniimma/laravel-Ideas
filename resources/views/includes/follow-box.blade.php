0803af8d6b76d8ca0285a258b1500ce3c9c148e1
Nima <alireza.khazforoosh@adventistas.org>
2024-06-25 17:15:37 -0300

    update follow-box

--- resources/views/includes/follow-box.blade.php
+++ resources/views/includes/follow-box.blade.php
@@ -4,18 +4,35 @@
     </div>
     <div class="card-body">
         @foreach ($topUsers as $user)
-            <div class="hstack gap-2 mb-3">
-                <div class="avatar">
-                    <a href="{{ route('users.show', $user->id) }}"><img width="50" class="avatar-img rounded-circle"
-                            src="{{ $user->getImageURL() }}" alt="{{ $user->name }}"></a>
+            <div class="d-flex justify-content-between">
+                <div class="hstack gap-2 mb-3">
+                    <div class="avatar">
+                        <a href="{{ route('users.show', $user->id) }}"><img width="50"
+                                class="avatar-img rounded-circle" src="{{ $user->getImageURL() }}"
+                                alt="{{ $user->name }}"></a>
+                    </div>
+                    <div class="overflow-hidden">
+                        <a class="h6 mb-0" href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a>
+                        <p class="mb-0 small text-truncate text-light">{{ $user->email }}</p>
+                    </div>
                 </div>
-                <div class="overflow-hidden">
-                    <a class="h6 mb-0" href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a>
-                    <p class="mb-0 small text-truncate text-light">{{ $user->email }}</p>
-                </div>
-                <a class="btn btn-primary-soft rounded-circle icon-md ms-auto" href="#"><i
-                        class="fa-solid fa-plus">
-                    </i></a>
+                @auth
+                    @if (Auth::id() !== $user->id)
+                        <div class="mt-3">
+                            @if (Auth::user()->follows($user))
+                                <form action="{{ route('users.unfollow', $user->id) }}" method="post">
+                                    @csrf
+                                    <button class="btn btn-danger btn-sm">Unfollow</button>
+                                </form>
+                            @else
+                                <form action="{{ route('users.follow', $user->id) }}" method="post">
+                                    @csrf
+                                    <button class="btn btn-primary btn-sm">Follow</button>
+                                </form>
+                            @endif
+                        </div>
+                    @endif
+                @endauth
             </div>
         @endforeach
     </div>
